5edc444e857ae0e164e8ae71ae641f46dd908d3c
antoinerenault53 <antoine.renault53@laposte.net>
2018-06-08 15:42:24 +0200

    PMD error

--- src/main/java/com/iia/calcultp/Utils.java
+++ src/main/java/com/iia/calcultp/Utils.java
@@ -3,36 +3,41 @@ package com.iia.calcultp;
 
 /**
  * Fonction global to project.
+ * 
  * @author kevin-pc
  *
  */
 public final class Utils {
 
-    private Utils() {
-        super();
-    }
-    
-    /**
-     * Print message in console.
-     *  @param message message a afficher
-     */
-    public static void message(final String message) {
-        System.out.print("\r\n" + message);
-    }
-    
-    /**
-     * Essaie de parser une string en int.
-     * @param value valeur à parser
-     * @return true si parsable , false si non parsable
-     */
-    public static boolean tryParseInt(final String value) {  
-        boolean result = false;
-           try {  
-            Integer.parseInt(value);  
-            result =  true;  
-         } catch (NumberFormatException e) {  
-             
-         } 
-        return result;
-   }
+	private Utils() {
+		super();
+	}
+
+	/**
+	 * Print message in console.
+	 * 
+	 * @param message
+	 *            message a afficher
+	 */
+	public static void message(final String message) {
+		System.out.print("\r\n" + message);
+	}
+
+	/**
+	 * Essaie de parser une string en int.
+	 * 
+	 * @param value
+	 *            valeur à parser
+	 * @return true si parsable , false si non parsable
+	 */
+	public static boolean tryParseInt(final String value) {
+		boolean result;
+		try {
+			Integer.parseInt(value);
+			result = true;
+		} catch (NumberFormatException e) {
+			result = false;
+		}
+		return result;
+	}
 }
