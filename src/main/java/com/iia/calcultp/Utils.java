ba9196f01849b48686473415ab5d54a03d74459f
PauchardThomas <thomaspauch@hotmail.fr>
2018-06-08 16:27:17 +0200

    Optimise Utils file for checkstyle

--- src/main/java/com/iia/calcultp/Utils.java
+++ src/main/java/com/iia/calcultp/Utils.java
@@ -9,35 +9,36 @@ package com.iia.calcultp;
  */
 public final class Utils {
 
-	private Utils() {
-		super();
-	}
+    /**
+     * Constructor
+     */
+    private Utils() {
+        super();
+    }
 
-	/**
-	 * Print message in console.
-	 * 
-	 * @param message
-	 *            message a afficher
-	 */
-	public static void message(final String message) {
-		System.out.print("\r\n" + message);
-	}
+    /**
+     * Print message in console.
+     * 
+     * @param message message a afficher
+     */
+    public static void message(final String message) {
+        System.out.print("\r\n" + message);
+    }
 
-	/**
-	 * Essaie de parser une string en int.
-	 * 
-	 * @param value
-	 *            valeur à parser
-	 * @return true si parsable , false si non parsable
-	 */
-	public static boolean tryParseInt(final String value) {
-		boolean result;
-		try {
-			Integer.parseInt(value);
-			result = true;
-		} catch (NumberFormatException e) {
-			result = false;
-		}
-		return result;
-	}
+    /**
+     * Essaie de parser une string en int.
+     * 
+     * @param value valeur à parser
+     * @return true si parsable , false si non parsable
+     */
+    public static boolean tryParseInt(final String value) {
+        boolean result = false;
+        try {
+            Integer.parseInt(value);
+            result = true;
+        } catch (NumberFormatException e) {
+            Utils.message(e.getMessage());
+        }
+        return result;
+    }
 }
