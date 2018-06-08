ed379ad8533398590113fbd772c7de4310451144
PauchardThomas <thomaspauch@hotmail.fr>
2018-06-08 15:55:26 +0200

    Suppression méthode inutile

--- src/main/java/com/iia/calcultp/Application.java
+++ src/main/java/com/iia/calcultp/Application.java
@@ -25,7 +25,8 @@ public final class Application {
        
         showMenu();
         
-        final String userInput = prompt("Veuillez saisir une option :");
+        Utils.message("Veuillez saisir une option : ");
+        final String userInput = new Scanner(System.in).nextLine();
         
         if ("q".equals(userInput)) {
             System.out.println("Au revoir!");
@@ -37,15 +38,6 @@ public final class Application {
                 Utils.message("Veuillez saisir une valeur entre 1 et 8 (q = quitter)");
             }
         }
-    }
-    /**
-     * Get User input string.
-     * @param promptMessage prompt description.
-     * @return user prompt.
-     */
-    public static String prompt(final String promptMessage) {
-        System.out.println(promptMessage);
-        return new Scanner(System.in).nextLine();
     }
     /**
      * Show application menu.
