a6f8f5ff565ca0f7c0519b011d8d33a8983e22b2
kevin <k.alonso@iia-laval.fr>
2018-06-08 16:02:38 +0200

    Deport selectOption in SelectOperation class

--- src/main/java/com/iia/calcultp/Application.java
+++ src/main/java/com/iia/calcultp/Application.java
@@ -32,7 +32,8 @@ public final class Application {
             System.exit(0);
         } else {
             if(Utils.tryParseInt(userInput)) {
-                selectOption(userInput);
+                //selectOption(userInput);
+                SelectOperation.selectOption(userInput);
             } else {
                 Utils.message("Veuillez saisir une valeur entre 1 et 8 (q = quitter)");
             }
@@ -55,104 +56,5 @@ public final class Application {
         System.out.print("7- sin cos tang\r\n");
         System.out.print("8- Historique des opérations\r\n");
     }
-    /**
-     * Select the option according user prompt.
-     * @param userInput.
-     */
-    public static void selectOption(final String userInput) {
-        
-        final int number1 = getNumber();
-        final int number2 = getNumber();
-        int result = 0;
-        boolean operationDone = true;
-        
-        switch (Integer.parseInt(userInput)) {
-        case 1:
-            // addition
-           try {
-               result = Operation.add(number1, number2);
-           } catch (NumberFormatException e) {
-               e.printStackTrace();
-           }
-           break;
-        case 2:
-            // soustraction
-            try {
-                result = Operation.substract(number1, number2);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
-            break;
-        case 3:
-            //multiplication
-            try {
-                result = Operation.multiply(number1, number2);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
-            break;
-        case 4:
-            //division
-            if(number2 == 0) {
-                Utils.message("Division par 0 impossible");
-                operationDone = false;
-            }else {
-                try {
-                    result = Operation.divide(number1, number2);
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
-                
-            }
-            break;
-        case 5:
-            // modulo
-            try {
-                result = Operation.modulo(number1, number2);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
-            break;
-        case 6:
-            // pourcentage
-            try {
-                result = Operation.pourcentage(number1);
-            } catch (NullPointerException e) {
-                e.printStackTrace();
-            }
-            break;
-        case 7:
-            // sin cos tang
-            Utils.message("En construction...");
-            operationDone = false;
-            break;
-        case 8:
-            // historique
-            Utils.message("En construction....");
-            operationDone = false;
-            break;
-        default:
-            // Choix invalide
-            Utils.message("Choix invalide");
-            operationDone = false;
-            break;
-        } 
-        if(operationDone) {
-            Utils.message("Résultat de l'opération : " + result);
-        }
-    }    
-    /**
-     * Add two value.
-     * @return result of addition.
-     */
-    public static int getNumber() {
-        String number;
-        do
-        {
-            Utils.message("Saisir un nombre : ");
-            number = new Scanner(System.in).nextLine();
-        }while(!Utils.tryParseInt(number));
-        
-        return Integer.parseInt(number);
-    }
+    
 }
