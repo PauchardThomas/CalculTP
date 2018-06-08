39b60819800cbb8723c40741a530ad32a792b1fd
antoinerenault53 <antoine.renault53@laposte.net>
2018-06-08 15:25:11 +0200

    Correction pmd TryParseInt

--- src/main/java/com/iia/calcultp/Utils.java
+++ src/main/java/com/iia/calcultp/Utils.java
@@ -3,24 +3,27 @@ package com.iia.calcultp;
 
 /**
  * Fonction global to project.
+ * 
  * @author kevin-pc
  *
  */
 public final class Utils {
 
-    private Utils() {
-        super();
-    }
-    
-    /**
-     * Print message in console.
-     *  @param message message a afficher
-     */
-    public static void message(final String message) {
-        System.out.print("\r\n" + message);
-    }
-    
-    /**
+	private Utils() {
+		super();
+	}
+
+	/**
+	 * Print message in console.
+	 * 
+	 * @param message
+	 *            message a afficher
+	 */
+	public static void message(final String message) {
+		System.out.print("\r\n" + message);
+	}
+
+	/**
      * Essaie de parser une string en int.
      * @param value valeur à parser
      * @return true si parsable , false si non parsable
@@ -31,7 +34,9 @@ public final class Utils {
             Integer.parseInt(value);  
             result =  true;  
          } catch (NumberFormatException e) {  
-            result = false;  
+            if(result != true)
+            {
+        	 result = false;  
          } 
         return result;
    }
