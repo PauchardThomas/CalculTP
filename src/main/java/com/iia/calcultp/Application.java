e606c6674f83f2729e804be77ba4a33014f7a8df
PauchardThomas <thomaspauch@hotmail.fr>
2018-06-12 14:19:14 +0200

    Add free operation

--- src/main/java/com/iia/calcultp/Application.java
+++ src/main/java/com/iia/calcultp/Application.java
@@ -3,6 +3,8 @@
 package com.iia.calcultp;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * My main Application.
@@ -23,6 +25,18 @@ public final class Application {
     private static double number2;
     /** Operation result. */
     private static double result;
+    /** Previous char. */
+    private static char previousChar = '\0';
+    /** operators list. */
+    private static String operatorList = "+-*/^";
+    /** parenthesis list. */
+    private static String parentheseList = "()";
+    /** Not found in indexof. */
+    private static final int NOT_FOUND = -1;
+    /** matcher. */
+    private static Matcher matcher;
+    /** pattern. */
+    private static Pattern pattern; 
 
     /**
      * Constructor.
@@ -39,7 +53,7 @@ public final class Application {
         showMenu();
         manageMenu(prompt());
     }
-    
+
     /**
      * Manage operation .
      * @param userOperationChoice user operation choice
@@ -52,32 +66,85 @@ public final class Application {
             number2 = getNumber();
             //result =  executeTwoNumbersOperation(userOpeChoice, number1, number2);
             result = SelectOperation.selectOptionWithTwoNumbers(userOpeChoice, number1, number2);
-         } else {
-            if(userOpeChoice <= MAX_ONE_NB_OPE) {
-                Utils.message("Saisir un nombre : ");
-                number1 = getNumber();
-                //result = executeOneNumberOperation(userOpeChoice, number1);
-                result = SelectOperation.selectOption(userOpeChoice, number1);
+        } else if (userOpeChoice <= MAX_ONE_NB_OPE) {
+            Utils.message("Saisir un nombre : ");
+            number1 = getNumber();
+            //result = executeOneNumberOperation(userOpeChoice, number1);
+            result = SelectOperation.selectOption(userOpeChoice, number1);
+
+        } else {
+            Utils.message("Saisir votre opération : ");
+            final String prompt = prompt();
+            if (isMathsExpression(prompt)) {
+                result = Calcul.eval(prompt);
+            } else {
+                System.out.print("Expression invalide");
+                result = -1;
             }
-            
-         }
+        }
         displayResultOperation(result);
         showSubMenu();
         manageSubMenu(userOpeChoice, prompt());
     }
-    
+
+    /**
+     * Check if user prompt is a valid math expression.
+     * @param str string to verify 
+     * @return boolean if is math expression
+     */
+    public static boolean isMathsExpression(final String str) {
+
+        boolean isValid;
+        pattern = Pattern.compile("((\\d*\\.\\d+)|(\\d+)|([\\+\\-\\*/\\(\\)\\^]))");
+        matcher = pattern.matcher(str);
+        final StringBuilder output = new StringBuilder();
+        while (matcher.find()) {
+            output.append(matcher.group());
+        }
+        if (output.length() == str.length() &&
+                output.length() != 0 && !isContainDoubleOperator(str)) {
+            isValid = true;
+        } else {
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    /**
+     * Check if String contain two consecutive operators.
+     * @param str string to verify
+     * @return boolean is contain double operator 
+     */
+    public static boolean isContainDoubleOperator(final String str) {
+        boolean isDoubleOperator = false;
+        for (final char ch: str.toCharArray()) {
+            
+            if (ch == previousChar && operatorList.indexOf(ch) != NOT_FOUND
+                    || (parentheseList.indexOf(ch) != NOT_FOUND && (parentheseList.indexOf(previousChar) != -1))
+                    || (operatorList.indexOf(previousChar) == NOT_FOUND && ch == '(')
+                    ) {
+                isDoubleOperator = true;
+                break;
+            }
+            
+            previousChar = ch;
+        }
+
+        return isDoubleOperator;
+    }
+
     /**
      * Display operation result.
      * @param result
      */
     public static void displayResultOperation(final double result) {
         if (result == -1) {
-            Utils.message("Pas de rÃ©sultat");
+            Utils.message("Pas de résultat");
         } else {
-            Utils.message("resultat de l'opÃ©ration : " + result); 
+            Utils.message("resultat de l'opération : " + result); 
         }
     }
-    
+
     /**
      * Exit the calculator.
      */
@@ -85,7 +152,7 @@ public final class Application {
         System.out.print("Au revoir!");
         System.exit(0);
     }
-    
+
     /**
      * Show application menu.
      */
@@ -93,7 +160,7 @@ public final class Application {
         System.out.print("\r\n*************************************** \r\n");
         System.out.print("* Bienvenue dans votre calculatrice ! * \r\n");
         System.out.print("*************************************** \r\n \r\n");
-        
+
         System.out.print("1- Addition \r\n");
         System.out.print("2- Soustraction\r\n");
         System.out.print("3- Multiplication\r\n");
@@ -103,9 +170,10 @@ public final class Application {
         System.out.print("7- sin\r\n");
         System.out.print("8- cos\r\n");
         System.out.print("9- tang\r\n");
-        System.out.print("10- Historique des opÃ©rations\r\n");
+        System.out.print("10- Historique des opérations\r\n");
+        System.out.print("11- Opération libre\r\n");
     }
-    
+
     /**
      * Manage menu.
      */
@@ -117,20 +185,20 @@ public final class Application {
                 final int userNumber = Integer.parseInt(userInput);
                 manageOperation(userNumber);
             } else {
-                Utils.message("Veuillez saisir une valeur entre 1 et 10 (q = quitter)");
+                Utils.message("Veuillez saisir une valeur entre 1 et 11 (q = quitter)");
                 manageMenu(prompt());
             }
         }
     }
-    
+
     /**
      * Show submenu.
      */
     public static void showSubMenu() {
         System.out.print("\r\n0- Retour au menu \r\n");
-        System.out.print("1- Nouvelle opÃ©ration\r\n");
+        System.out.print("1- Nouvelle opération\r\n");
     }
-    
+
     /**
      * Manage submenu.
      * @param currentOperation currentOperation
@@ -155,17 +223,17 @@ public final class Application {
             manageSubMenu(currentOperation, prompt());
         }
     }
-    
+
     /**
      * Get user prompt number.
      * @return user prompt number
      */
     public static double getNumber() {
         do {
-           number = prompt();
-           if (number.contains(",")) {
-               number = number.replaceAll(",", ".");
-           }
+            number = prompt();
+            if (number.contains(",")) {
+                number = number.replaceAll(",", ".");
+            }
         }while(!Utils.tryParseDouble(number));
 
         return Double.parseDouble(number);
--- src/test/java/com/iia/calcultp/ApplicationTest.java
+++ src/test/java/com/iia/calcultp/ApplicationTest.java
@@ -32,7 +32,8 @@ public class ApplicationTest {
                 + "7- sin\r\n"
                 + "8- cos\r\n"
                 + "9- tang\r\n"
-                + "10- Historique des opÃ©rations\r\n", os.toString());
+                + "10- Historique des opérations\r\n"
+                + "11- Opération libre\r\n", os.toString());
     }
     
     @org.junit.Test
@@ -44,7 +45,7 @@ public class ApplicationTest {
 
         Application.showSubMenu();
         Assert.assertEquals("\r\n0- Retour au menu \r\n"
-                + "1- Nouvelle opÃ©ration\r\n", os.toString());
+                + "1- Nouvelle opération\r\n", os.toString());
     }
     
     @org.junit.Test
@@ -58,7 +59,49 @@ public class ApplicationTest {
         
         Application.displayResultOperation(result);
         
-        Assert.assertEquals("\r\nresultat de l'opÃ©ration : " + result, os.toString());
+        Assert.assertEquals("\r\nresultat de l'opération : " + result, os.toString());
+    }
+    
+    @org.junit.Test
+    public void testIsContainDoubleOperator() {
+        String str = "2(3)";
+        boolean result = Application.isContainDoubleOperator(str);
+        Assert.assertEquals(result,true);
+        
+        String str2 = "2*(4-3)";
+        boolean result2 = Application.isContainDoubleOperator(str2);
+        Assert.assertEquals(result2,false);
+        
+        String str3 = "(2+3)(4-3)";
+        boolean result3 = Application.isContainDoubleOperator(str3);
+        Assert.assertEquals(result3,true);
+        
+        String str4 = "2+3*4";
+        boolean result4 = Application.isContainDoubleOperator(str4);
+        Assert.assertEquals(result4,false);
+    }
+    
+    @org.junit.Test
+    public void testIsMathsExpression() {
+        String str = "2**2";
+        boolean result = Application.isMathsExpression(str);
+        Assert.assertEquals(result,false);
+        
+        String str2 = "p*p";
+        boolean result2 = Application.isMathsExpression(str2);
+        Assert.assertEquals(result2,false);
+        
+        String str3 = "2+3-77pk";
+        boolean result3 = Application.isMathsExpression(str3);
+        Assert.assertEquals(result3,false);
+        
+        String str4 = "4*6-4+3/7";
+        boolean result4 = Application.isMathsExpression(str4);
+        Assert.assertEquals(result4,true);
+        
+        String str5 = "2(3)";
+        boolean result5 = Application.isMathsExpression(str5);
+        Assert.assertEquals(result5,false);
     }
     
   /*  @org.junit.Test
