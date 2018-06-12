c177b57fef7b1d1eaf7791eff6ee45f1fced94ea
kevin <k.alonso@iia-laval.fr>
2018-06-12 14:49:28 +0200

    test

--- src/main/java/com/iia/calcultp/Application.java
+++ src/main/java/com/iia/calcultp/Application.java
@@ -189,10 +189,8 @@ public final class Application {
      * Show submenu.
      */
     public static void showSubMenu() {
-        logger.info("Start submenu");
         System.out.print("\r\n0- Retour au menu \r\n");
         System.out.print("1- Nouvelle opération\r\n");
-        logger.info("End submenu");
     }
     /**
      * Constructor.
