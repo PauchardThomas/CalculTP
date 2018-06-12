28cef65894c372193caac5c7d245704e0b72cca0
antoinerenault53 <antoine.renault53@laposte.net>
2018-06-12 14:10:10 +0200

    Correction error PMD

--- src/main/java/com/iia/calcultp/Application.java
+++ src/main/java/com/iia/calcultp/Application.java
@@ -46,8 +46,7 @@ public final class Application {
 
     public static void main(final String[] args) {
         logger.info("Start application");
-        Historical.initFile();
-        //Historical.listFile();
+        Historical.initFile();;
         showMenu();
         manageMenu(prompt());
     }
